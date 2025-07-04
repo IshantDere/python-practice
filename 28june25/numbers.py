fb22c01d1090039d3f313e8c239517433a5682e9
IshantDere <Ishantdere06@gmail.com>
2025-07-04 21:18:26 +0530

    Function in numbers

---
+++ 28june25/numbers.py
@@ -0,0 +1,22 @@
+fav_num = 4
+print(str(fav_num) + " is my number")
+
+# ------or------
+
+print(fav_num, "is my number")
+
+my_num = -8
+print(abs(my_num))
+# abs function gives the absolute value of any number
+
+print(pow(4, 2)) #<----- i.e. 4^2 = 16
+# pow is use to find any numbers power
+
+print(max(2, 4, 11))
+# it is use to find maximum number
+
+print(min(3, 7, 9))
+# it is used to find the minimum number
+
+print(round(4.2))
+# it is used to round off the number

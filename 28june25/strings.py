1835ee7b29fef74cae804930ad38ea51e86e19fa
IshantDere <Ishantdere06@gmail.com>
2025-07-04 21:17:07 +0530

    String function

---
+++ 28june25/strings.py
@@ -0,0 +1,28 @@
+print("tutorial video")
+# inside the ("") are known as strings
+
+print("tutorial\nvideo")
+# it insert a line 
+
+print("tutorial\"video")
+# it adds a (") mark in between two characters
+
+x = "Ishant dere"
+print(x.lower())
+# This lower function converts the word to lowercase
+print(x.upper())
+# This upper function converts the word to uppercase
+print(x.islower())
+# This function is tells us about the word is a lowercase or not
+print(x.isupper())
+# This function tells us about the word is uppercase or not 
+print(x.lower().islower())
+# we have conveted the word into lower and then we have checked either it lower or not
+print(len(x))
+# len function tells us about the number of characters are present in the string
+print(x[0])
+# the above function used to grab a sphesific co-ordinate character from the string 
+print(x.index("t"))
+# this function used to identify any character from the string by using the index 
+print(x.replace("Ishant", "vedant"))
+# to replace anything we have to put 1st what we have to rplace and 2nd to replace with what

0f36cf74a9afd5e77927ed1241a8bb672ff0e0d3
IshantDere <Ishantdere06@gmail.com>
2025-07-17 21:01:05 +0530

    added a basic commands of the lists

---
+++ 28june25/lists.py
@@ -0,0 +1,12 @@
+colors = ["red", "blue", "black"]
+print(colors)
+
+print(colors[1:2]) #it helps us to access any element from the list by using the index
+
+colors[1] = "pink"
+print(colors[1])
+# it replaces the element
+
+l1 = [2,3,4,5,6,7]
+l1.append(10)
+print(l1)

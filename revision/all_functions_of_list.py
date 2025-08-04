6802f5e8c0a91d3ce0938a2427c4a519734024a8
IshantDere <Ishantdere06@gmail.com>
2025-08-04 20:44:36 +0530

    added a multiple syntaxes of functions of lists

---
+++ revision/all_functions_of_list.py
@@ -0,0 +1,60 @@
+number1 = [12, 3, 50, 42, 20, 4]
+frnds = ["ishant", "vedant", "vibha", "devansh", "shriyansh"]
+
+# print(number1, frnds)
+
+
+# --------------------------------------   ALL FUNCTIONS OF LISTS   --------------------------------------------- #
+
+# 1) extend function 
+
+## frnds.extend(number1)
+# print(frnds)
+
+# 2) append function 
+
+## frnds.append("ajinkya")
+# print(frnds)
+
+# 3) insert function
+
+## frnds.insert(1, "prajakta")
+# print(frnds)
+
+# 4) remove function
+
+## frnds.remove("ishant")
+# print(frnds)
+
+# 5) clear function
+
+## frnds.clear()
+# print(frnds)
+
+# 6) pop function 
+
+## frnds.pop()
+# print(frnds)
+
+# 7) index function 
+
+# print(frnds.index("devansh"))
+
+# 8) count function (it tells how many element for the "input" are available in the list)
+
+# print(frnds.count("vedant"))
+
+# 9) sort function(it sort the elements of list in the asending order)
+
+## frnds.sort()
+# print(frnds)
+
+# 10) reverse function
+
+## frnds.reverse()
+# print(frnds)
+
+# 11) copy function 
+
+## friends2 = frnds.copy()
+# print(friends2)

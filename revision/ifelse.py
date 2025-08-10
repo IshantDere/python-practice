60aa327ba08e0f2aca09f03d6d2fcee2c227a90d
IshantDere <Ishantdere06@gmail.com>
2025-08-10 23:09:54 +0530

    added a basic number problem by using if else

---
+++ revision/ifelse.py
@@ -0,0 +1,14 @@
+num = int(input("Enter the first number: "))
+
+if num < 5:
+    print("the number is less than 5")
+
+elif num > 5:
+    print("the number is greater than 5")
+
+elif num == 0:
+    print("the number is equal to 0")
+
+else:
+    print("the input is wrong")    
+         
